f98a5a6b45c7ad0180ee3f098236361ff3be0134
ElielOJr <elieljr.pro@outlook.com>
2024-11-16 15:49:55 -0300

    First struct grid

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Load the page")
+
+    var section = document.getElementById("rectangle");
+
+    for (var i = 0; i<=104; i++){
+        var div = document.createElement("div");
+    div.className = "SubDiv";
+    div.id = "SubDiv"
+    section.appendChild
+    section.appendChild(div)
+    }
+    
+});
