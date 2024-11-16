f8e1862cc98bee7141c3035eee6812a3b8405244
ElielOJr <elieljr.pro@outlook.com>
2024-11-16 16:21:16 -0300

    Config Div click

--- script.js
+++ script.js
@@ -3,12 +3,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var section = document.getElementById("rectangle");
 
-    for (var i = 0; i<=104; i++){
-        var div = document.createElement("div");
-    div.className = "SubDiv";
-    div.id = "SubDiv"
-    section.appendChild
-    section.appendChild(div)
-    }
+    var gridLength = 104
+    var gridStatePosition = generateArrayStats(gridLength)
+
+    createSmallBoxs(section, gridLength)
+    console.log(gridStatePosition)
     
+    addClickListenner()
 });
+
+function createSmallBoxs(section, amount) {
+    for (var i = 0; i <= amount; i++) {
+        var div = document.createElement("div");
+        div.className = "SubDiv";
+        div.id = i
+        section.appendChild(div)
+    }
+}
+
+function generateArrayStats(arraySize) {
+    var array = []
+    for (var i = 0; i <= arraySize; i++) {
+        array.push("false")
+    }
+    return array
+}
+
+function addClickListenner() {
+    var divs = document.querySelectorAll(".SubDiv");
+    
+    divs.forEach(function(div) {
+        div.addEventListener("click", function() {
+            alert("Div " + this.id + " foi clicada!");
+        });
+    });
+}
